4e34b0f75d26a6946cf95a8fe867d5d1ed7d73b0
revett <charlierevett@gmail.com>
2016-11-28 12:21:42 +0000

    Move to new testing format

--- backoff_test.go
+++ backoff_test.go
@@ -6,42 +6,44 @@ import (
 	"time"
 )
 
-func TestNew(t *testing.T) {
-	intervals := []int{0, 200, 400}
-	logPrefix := "[example]"
+func TestPolicy(t *testing.T) {
+	t.Run(".{}", func(t *testing.T) {
+		intervals := []int{0, 200, 400}
+		logPrefix := "[example]"
 
-	bp := Policy{
-		Intervals: intervals,
-		LogPrefix: logPrefix,
-	}
+		bp := Policy{
+			Intervals: intervals,
+			LogPrefix: logPrefix,
+		}
 
-	if bp.Intervals == nil {
-		t.Error("Intervals not accessible.")
-	}
+		if bp.Intervals == nil {
+			t.Error("Intervals not accessible.")
+		}
 
-	if bp.LogPrefix == "" {
-		t.Error("LogPrefix not accessible.")
-	}
-}
+		if bp.LogPrefix == "" {
+			t.Error("LogPrefix not accessible.")
+		}
+	})
 
-func TestPerform(t *testing.T) {
-	intervals := []int{1000, 1000}
+	t.Run(".Perform", func(t *testing.T) {
+		intervals := []int{1000, 1000}
 
-	bp := Policy{
-		Intervals: intervals,
-		LogPrefix: "",
-	}
+		bp := Policy{
+			Intervals: intervals,
+			LogPrefix: "",
+		}
 
-	anon := func() bool {
-		fmt.Println("Connecting...")
-		return false
-	}
+		anon := func() bool {
+			fmt.Println("Connecting...")
+			return false
+		}
 
-	start := time.Now()
-	_ = bp.Perform(anon)
-	end := time.Since(start).Seconds()
+		start := time.Now()
+		_ = bp.Perform(anon)
+		end := time.Since(start).Seconds()
 
-	if end < 1 || end > 3 {
-		t.Error(".Perform() did not sleep for the correct duration.")
-	}
+		if end < 1 || end > 3 {
+			t.Error(".Perform() did not sleep for the correct duration.")
+		}
+	})
 }
